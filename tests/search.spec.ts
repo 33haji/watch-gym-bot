d3345361cef8ee4931370c892d43ec00551f7eba
33haji <one_for_all_0828@yahoo.co.jp>
2022-08-02 10:02:20 +0900

    表示する日付を増やすをクリックまで

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -9,4 +9,24 @@ test('平日の夜に空きがあるかどうかチェック', async ({ page }) 
 
   // 「利用者ページへ」をクリック
   await page.locator('input[title="利用者ページへ"]').click();
+
+  // IDとPASSを入力して「確定」をクリック
+  const id = process.env.USER_ID as string;
+  const password = process.env.USER_PASSWORD as string;
+  await page.locator('input[name="g_riyoushabangou"]').fill(id);
+  await page.locator('input[name="ansyono"]').fill(password);
+  await page.locator('input[title="確定"]').click();
+
+  // 「予約申込」をクリック
+  await page.locator('input[title="予約申込"]').click();
+
+  // 「屋内スポーツ施設」 → 「四谷スポーツスクエア」 → 「四谷スポーツスクエア多目的ホール」 → 「バレーボール」 → 「確定」の順にクリック
+  await page.locator('a[title="屋内スポーツ施設"]').click();
+  await page.locator('a[title="四谷スポーツスクエア"]').click();
+  await page.locator('a[title="四谷スポーツスクエア多目的ホール"]').click();
+  await page.locator('a[title="バレーボール"]').click();
+  await page.locator('input[title="確定"]').click();
+
+  // 「表示する日付を増やす」をクリック
+  await page.locator('img[title="施設別に切替"]').click();
 });
