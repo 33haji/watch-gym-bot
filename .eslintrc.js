babdab9ab1f4dbe217f4d341911def3b421fd365
33haji <one_for_all_0828@yahoo.co.jp>
2022-08-08 09:56:37 +0900

    セミコロンとシングルクオートのeslintのルールを追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   root: true,
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single']
+  }
 };
