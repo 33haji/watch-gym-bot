996b000fd1618d1103b70e26e1a524524e80a692
33haji <one_for_all_0828@yahoo.co.jp>
2022-12-08 14:45:40 +0900

    1/22をskipに

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 import { postMessageToSlack } from '../utils';
 
 // skipする日付
-const SKIP_DATES = ['12/25', '1/4', '1/7'];
+const SKIP_DATES = ['12/25', '1/4', '1/7', '1/22'];
 // 祝日
 const HOLIDAYS = ['12/28'];
 
