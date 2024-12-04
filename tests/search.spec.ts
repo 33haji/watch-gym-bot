3824434f7a51f3f7675c99ed8af048b72ea73291
33haji <one_for_all_0828@yahoo.co.jp>
2024-12-04 23:49:39 +0900

    12/22をskipに

--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -2,9 +2,9 @@ import { test } from '@playwright/test';
 import { postMessageToSlack } from '../utils';
 
 // skipする日付
-const SKIP_DATES = ['11/4', '11/17'];
+const SKIP_DATES = ['12/22'];
 // 祝日
-const HOLIDAYS = ['11/4', '1/13', '2/11', '2/24', '3/20'];
+const HOLIDAYS = ['1/13', '2/11', '2/24', '3/20'];
 
 test('空きがあるかどうかチェック', async ({ page }) => {
   // 対象の月の中で空きがあるかチェックする関数
